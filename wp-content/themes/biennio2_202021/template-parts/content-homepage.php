7b7bd3953b1fba88c05f56d2c2eec18a94a69c8d
Luca Bravo <>
2021-01-13 10:58:38 +0100

    first commit + initial parametrization

---
+++ wp-content/themes/biennio2_202021/template-parts/content-homepage.php
@@ -0,0 +1,6 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="page__header">
+		<h1 class="page__title"><?php the_title(); ?></h1>
+	</header>
+	<div class="page__content"><?php the_content(); ?></div>
+</article>
